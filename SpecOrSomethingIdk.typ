f0f8e5c341d30867285015c22bf4736b6939c771
artemis <art06and@gmail.com>
2023-05-17 21:39:43 +0200

    i am suffering at my inability to just write a parser

---
+++ SpecOrSomethingIdk.typ
@@ -0,0 +1,38 @@
+= Tara
+== Syntax
+Very basic snippet to establish tara's syntactical goals.
+```tara
+fn main(args: *[str]): int ->
+	for a in args -> 
+		println(a);
+	0.
+```
+As such some central guidelines are:
+```peg
+programm   <- function*
+function   <- "fn" name "(" paramList? ")" typeSig? "->" exprList? ("." | ";")
+// the structure here is such, that the last parsed parameter
+// is treated specially, as it can be important to easily access
+// the last element of such a list for later semantic analysis
+paramList  <- (parameter ",")* parameter ","?
+parameter  <- name typeSig
+typeSig    <- ":" name
+// here for example the last expr is returned from a function
+// unless a semicolon is used to end the block
+exprList   <- ((normExpr ",") | (blockExpr ","?))* (normExpr | blockExpr) ","?
+blockExpr  <- ifStmt | matchStmt | forStmt
+ifStmt     <-
+matchStmt  <-
+forStmt    <- "for" name "in" normExpr "->" exprList? ("." | ";")
+normExpr   <- arithmetic | dataDecl | literal | name
+literal    <- numLit | strLit | charLit | boolLit
+numLit     <- ('0'..'9')+
+strLit     <- "\"" any* "\""
+charLit    <- "'" any "'"
+boolLit    <- "true" | "false"
+arithmetic <-
+dataDecl   <-
+name       <- alpha+
+alpha      <- 'a' .. 'Z'
+```
+== Semantics
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,82 @@
-mod comb_parser;
-mod parser;
-mod tokenizer;
-//use tokenizer::*;
+use std::ops::Deref;
+
+type Result<'a> = std::result::Result<(AST, Str<'a>), ParseError>;
+
+enum ParseError {
+    EndOfString,
+}
+
+enum AST {
+    Prog(Vec<Self>),
+}
+
+#[derive(Clone, Copy)]
+struct Str<'a> {
+    string: &'a str,
+    index: usize,
+    pos: (usize, usize),
+}
+impl<'a> Str<'a> {
+    fn advance(&mut self, x: usize) {
+        self.pos.0 += self.string[self.index..self.index + x]
+            .chars()
+            .map(|c| if c == '\n' { 1 } else { 0 })
+            .sum::<usize>();
+        if let Some(i) = self.string[self.index..self.index + x].rfind('\n') {
+            self.pos.1 = x - i;
+        } else {
+            self.pos.1 += x;
+        }
+        self.index += x;
+    }
+    fn check(&mut self, start: &str) -> bool {
+        if self.starts_with(start) {
+            self.advance(start.len());
+            true
+        } else {
+            false
+        }
+    }
+}
+
+impl<'a> Deref for Str<'a> {
+    type Target = str;
+
+    fn deref(&self) -> &Self::Target {
+        &self.string[self.index..]
+    }
+}
+
+// programm   <- function*
+fn parse_programm(input: Str) -> Result {
+    let mut acc = vec![];
+    let mut head = input;
+    while let Ok((a, s)) = parse_function(head) {
+        head = s;
+        acc.push(a);
+    }
+    return Ok((AST::Prog(acc), head));
+}
+
+// function   <- "fn" name %todo from here% "(" paramList? ")" typeSig? "->" exprList? ("." | ";")
+fn parse_function(mut input: Str) -> Result {
+    let name;
+    if !input.check("fn") {
+        todo!();
+        //return
+    }
+    match parse_name(input) {
+        Ok((a, s)) => {
+            input = s;
+            name = a;
+        }
+        Err(e) => {
+            todo!()
+            //return
+        }
+    }
+}
 
 fn main() {
-    // let s =
-    // "fn main(argv: [Str]): int -> \n\t10 + 9 == 21\nend\nfn main2(argv: [Str]): int ->\n\t 10 `add` 9 ` eq` 21\nend\n";
-    //println!("{s}");
-    //println!("----");
-    //println!("{:?}", tokenize(s).expect("tokenizer failed!"));
-    //println!("{:?}", parser::parse("fn main -> damn end\n"));
-    parser::parse("fn main -> damn end\n").unwrap();
+    println!("Hello, world!");
 }
